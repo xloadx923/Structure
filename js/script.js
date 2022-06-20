d7730745b2a40965cacf17c7ca00fd353848d76c
Yann Guichard <xloadx923@msn.com>
2022-06-21 00:07:04 +0200

    image events css

--- js/script.js
+++ js/script.js
@@ -2,30 +2,31 @@ const divInterns = document.querySelectorAll(".divIntern") || document.querySele
 const buttonCollapse = document.querySelector(".buttonCollapse");
 const buttonRefresh = document.querySelector(".buttonRefresh");
 
-divInterns.forEach(function(divIntern){
+// divInterns.forEach(function(divIntern){
 
-    divIntern.addEventListener("mouseenter", function(event){
-        if(this.children[1]) this.children[1].remove();
+//     divIntern.addEventListener("mouseenter", function(event){
+//         if(this.children[1]) this.children[1].remove();
 
-        const preDescription = document.createElement('div');
-        preDescription.classList.add('preDescription');
-        const imageDescription = document.createElement('div');
-        imageDescription.classList.add('imageDescription');
-        imageDescription.append(this.children[0].dataset.description);
-        preDescription.append(imageDescription);
+//         const preDescription = document.createElement('div');
+//         preDescription.classList.add('preDescription');
+//         const imageDescription = document.createElement('div');
+//         imageDescription.classList.add('imageDescription');
+//         imageDescription.append(this.children[0].dataset.description);
+//         preDescription.append(imageDescription);
 
-        this.appendChild(preDescription);
-    });
-    divIntern.addEventListener("mouseleave", function(event){
-        this.children[1].remove();
-    });
-});
+//         this.appendChild(preDescription);
+//     });
+//     divIntern.addEventListener("mouseleave", function(event){
+//         this.children[1].remove();
+//     });
+// });
 
 /******************** BUTTONS ***********************/
 
 buttonCollapse.addEventListener("click", function(){
-    divInterns.forEach((divIntern) => divIntern.children[0].classList.toggle('active'));
+    divInterns.forEach((divIntern) => divIntern.children[0].children[0].classList.toggle('active'));
 });
 buttonRefresh.addEventListener("click", function(){
     location.reload();
 });
+divInterns[0].addEventListener("click", event => event.preventDefault());
